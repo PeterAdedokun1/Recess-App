df9869ab16a1f9d5cefa465fdb27a5cc33ab0765
peterAdedokun1 <peteradedokun436@gmail.com>
2023-07-19 21:04:30 +0100

    signup header

--- app/index.js
+++ app/index.js
@@ -1,11 +1,32 @@
 import React from 'react'
-import {View,Text} from "react-native"
+import { View, Text, SafeAreaView,StyleSheet } from "react-native"
+import { Stack, useRouter } from "expo-router"
+import { Logo } from '../component'
+import { SignUp } from './auth/SignUp'
 const index = () => {
   return (
-      <View>
-          <Text>my first app</Text>
-    </View>
-  )
+    <SafeAreaView style={{ flex: 1, backgroundColor: "white",  }}>
+      <Stack.Screen
+        
+        options={{
+          headerStyle: {
+            // backgroundColor: "white",
+          },
+          headerShadowVisible: false,
+          headerTitle: "",
+        
+          headerLeft: () => <Logo />,
+          headerRight: () => <SignUp />,
+        }}
+      ></Stack.Screen>
+    </SafeAreaView>
+  );
 }
 
 export default index
+
+const styles = StyleSheet.create({
+  heading: {
+      backgroundColor:"red"
+    }
+})
