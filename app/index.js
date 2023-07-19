e4917bee3fae4d3c12f8a2b6608f7edfd9859adc
peterAdedokun1 <peteradedokun436@gmail.com>
2023-07-19 14:11:15 +0100

    intial commit

---
+++ app/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {View,Text} from "react-native"
+const index = () => {
+  return (
+      <View>
+          <Text>my first app</Text>
+    </View>
+  )
+}
+
+export default index
