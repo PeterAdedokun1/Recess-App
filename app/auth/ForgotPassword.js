20cd46683bb87633d74b2b2ac02a3b81394e6dad
peterAdedokun1 <peteradedokun436@gmail.com>
2023-07-21 01:33:10 +0100

    fixes

--- app/auth/ForgotPassword.js
+++ app/auth/ForgotPassword.js
@@ -4,15 +4,15 @@ import {
   SafeAreaView,
   StyleSheet,
   TouchableOpacity,
-    TextInput,
-  Image
+  TextInput,
+  Image,
 } from "react-native";
 
 import { Stack, useRouter } from "expo-router";
 import { Back, Button, Input } from "../../component";
 import { ButtonStyles } from "../../styles/ButtonStyles";
 import { InputStyles } from "../../styles/InputStyles";
-import password from "../../assest/images/password.png";
+import mail from "../../assest/images/mail.png";
 const ForgotPassword = () => {
   const router = useRouter();
   return (
@@ -33,21 +33,19 @@ const ForgotPassword = () => {
       <View style={{ marginHorizontal: 10 }}>
         <View>
           <Text style={styles.headerText}>Enter your registered email</Text>
-          {/* <Input title="Email" placeholder="Enter Email" /> */}
-          {/* <Button text={"Send Reset"} /> */}
           <View style={InputStyles.inputContainer}>
             <Text style={InputStyles.inputText}>Email</Text>
             <View style={InputStyles.smallContainer}>
               <TextInput
                 style={InputStyles.input}
-                              placeholder="Enter your Email"
-                              keyboardType="email-address"
+                placeholder="Enter your Email"
+                keyboardType="email-address"
               />
               <Image
                 style={{
                   marginBottom: 20,
                 }}
-                source={password}
+                source={mail}
               />
             </View>
           </View>
