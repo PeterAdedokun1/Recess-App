885cbecc5c943a97b1cb0e1f0ae13155d5134969
peterAdedokun1 <peteradedokun436@gmail.com>
2023-07-22 23:55:21 +0100

    update

--- firebase/error.js
+++ firebase/error.js
@@ -1,3 +1,6 @@
 export const FIREBASE_ERRORS = {
-    "Firebase: Error (auth/email-already-in-use).": "A user with that email already exist"
-}
+  "Firebase: Error (auth/email-already-in-use).":
+    "A user with that email already exist",
+  "Firebase: Error (auth/user-not-found).": "Invalid email or password",
+  "Firebase: Error (auth/wrong-password).": "Invalid email or password",
+};
