c1d659711d3dbd49cb0fc4776441a32b405e01e3
peterAdedokun1 <peteradedokun436@gmail.com>
2023-07-25 20:45:13 +0100

    user section

--- app/dashboard/Home.js
+++ app/dashboard/Home.js
@@ -1,12 +1,24 @@
 import { View,Text ,SafeAreaView,TouchableOpacity} from 'react-native'
 import React from 'react'
+import { Stack } from 'expo-router';
 
 const Home = () => {
   return (
-    <View>
-      <Text>this is hoe page</Text>
-    </View>
-  )
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#24E4D9" }}>
+      <Stack.Screen
+        options={{
+          headerStyle: {
+            backgroundColor: "#24E4D9",
+          },
+          headerShadowVisible: false,
+          headerBackVisible: false,
+          // headerLeft: () => 
+          // headerRight: () => 
+        }}
+
+      />
+    </SafeAreaView>
+  );
 }
 
 export default Home
---
+++ component/UserSection.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Image,Text } from "react-native";
+
+const UserSection = () => {
+  return (
+      <View>
+          <Text>user section</Text>
+    </View>
+  )
+}
+
+export default UserSection
